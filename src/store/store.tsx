12e48d23986845da1644b1552b9b19ae2d6f0e11
AsrarUddin <asraruddin000@gmail.com>
2025-05-18 23:29:57 +0530

    habit-tracker

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -11,12 +11,13 @@ interface Habit {
 interface HabitState {
   habits: Habit[];
   addHabit: (name: string, frequency: "daily" | "weekly") => void;
-  removeHabit:(id:string) => void ;
-  toggleHabit:(id:string,date:string)=>void;
+  removeHabit: (id: string) => void;
+  toggleHabit: (id: string, date: string) => void;
 }
 
 const useHabitStore = create<HabitState>((set) => ({
   habits: [],
+
   addHabit: (name, frequency) =>
     set((state) => ({
       habits: [
@@ -29,9 +30,27 @@ const useHabitStore = create<HabitState>((set) => ({
           createdAt: new Date().toISOString(),
         },
       ],
-    })
-),
-})
-);
+    })),
+
+  removeHabit: (id) =>
+    set((state) => ({
+      habits: state.habits.filter((habit) => habit.id !== id),
+    })),
+
+  toggleHabit: (id, date) =>
+    set((state) => ({
+      habits: state.habits.map((habit) => {
+        if (habit.id !== id) return habit;
+
+        const alreadyCompleted = habit.completedDates.includes(date);
+        return {
+          ...habit,
+          completedDates: alreadyCompleted
+            ? habit.completedDates.filter((d) => d !== date)
+            : [...habit.completedDates, date],
+        };
+      }),
+    })),
+}));
 
 export default useHabitStore;
